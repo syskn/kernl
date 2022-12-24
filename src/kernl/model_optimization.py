cc8818f9bc113a728d7b485adfb128b0cf6eb440
syskn <bit192@gmail.com>
2022-12-25 05:39:01 +0900

    test

--- src/kernl/model_optimization.py
+++ src/kernl/model_optimization.py
@@ -26,6 +26,7 @@ from kernl.optimizer.dynamo_backend import dynamo_backend_ofi
 # https://github.com/pytorch/torchdynamo/issues/1816
 def _compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
     dynamo_backend_ofi(gm)
+    return gm
     return cuda_graphs_wrapper(gm, example_inputs)
 
 
